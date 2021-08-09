e735ce7e66ba731e8b7f4817545666ea3a8487ff
Wasymir <72921498+Wasymir@users.noreply.github.com>
2021-08-09 19:38:22 +0200

    added training loop

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ train_dataset = CIFAR10(root="data", download=True, train=True, transform=ToTens
 
 test_dataset = CIFAR10(root="data", download=True, train=False, transform=ToTensor())
 
-BATCH_SIZE = 100
+BATCH_SIZE = 1000
 
 train_data, test_data = DataLoader(train_dataset, BATCH_SIZE), DataLoader(
     test_dataset, BATCH_SIZE
@@ -21,11 +21,11 @@ train_data, test_data = DataLoader(train_dataset, BATCH_SIZE), DataLoader(
 class NN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Sequential(nn.ZeroPad2d(2), nn.Conv2d(3, 16, 5), nn.ReLU())
+        self.conv1 = nn.Sequential(nn.ZeroPad2d(2), nn.Conv2d(3, 32, 5), nn.ReLU())
         self.pool1 = nn.MaxPool2d(2)
-        self.conv2 = nn.Sequential(nn.ZeroPad2d(2), nn.Conv2d(16, 32, 5), nn.ReLU())
+        self.conv2 = nn.Sequential(nn.ZeroPad2d(2), nn.Conv2d(32, 64, 5), nn.ReLU())
         self.pool2 = nn.MaxPool2d(2)
-        self.output = nn.Linear(32 * 8 * 8, 10)
+        self.output = nn.Linear(64 * 8 * 8, 10)
 
     def forward(self, input):
         x = self.conv1(input)
@@ -33,6 +33,30 @@ class NN(nn.Module):
         x = self.conv2(x)
         x = self.pool2(x)
         x = torch.flatten(x, start_dim=1)
+        x = self.output(x)
         return x
+
+
+LR = 0.01
+N_EPOCHS = 10
+
+model = NN().to(device)
+optimizer = optim.Adam(model.parameters(), LR)
+loss_fn = nn.CrossEntropyLoss()
+
+for epoch in range(N_EPOCHS):
+    print(f"Epoch: {epoch + 1} / {N_EPOCHS}")
+    for step, (input, target) in enumerate(train_data):
+        input, target = input.to(device), target.to(device)
+        output = model(input)
+        output = output.to(device)
+        optimizer.zero_grad()
+        loss = loss_fn(output, target)
+        loss = loss.to(device)
+        loss.backward()
+        optimizer.step()
+        print(f"Step: {step}; Loss: {loss.item()}")
+
+
 
 
