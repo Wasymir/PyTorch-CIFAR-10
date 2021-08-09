6561f9c6d2f2b37a5774bfa01eac77ee6ed45475
Wasymir <72921498+Wasymir@users.noreply.github.com>
2021-08-09 19:05:00 +0200

    added imports & datasets to model.py

--- model.py
+++ model.py
@@ -1,1 +1,20 @@
-"" 
+import torch
+from torch import nn
+from torch import optim
+from torchvision.datasets import CIFAR10
+from torchvision.transforms import ToTensor
+
+train_dataset = CIFAR10(
+    root="data",
+    download=True,
+    train=True,
+    transform=ToTensor()
+)
+
+test_dataset = CIFAR10(
+    root="data",
+    download=True,
+    train=False,
+    transform=ToTensor()
+)
+
