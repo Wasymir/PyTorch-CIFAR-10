2bd43eb184304de5d822f5014df0ba6530a17e7b
Wasymir <72921498+Wasymir@users.noreply.github.com>
2021-08-17 23:43:44 +0200

    added batchnorm, one fc layer with dropout & train accuracy counter in training loop

--- model.py
+++ model.py
@@ -2,7 +2,6 @@ from numpy import mod
 import torch
 from torch import nn
 from torch import optim
-from torchvision import transforms
 from torchvision.datasets import CIFAR10
 from torchvision.transforms import ToTensor, Compose, RandomHorizontalFlip, RandomInvert, RandomVerticalFlip
 from torch.utils.data import DataLoader
@@ -12,7 +11,7 @@ from progress.bar import Bar
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-AUGMENTATION_PROBABILITY = 0.2
+AUGMENTATION_PROBABILITY = 0.5
 
 train_transform = Compose([
     RandomHorizontalFlip(AUGMENTATION_PROBABILITY),
@@ -35,18 +34,20 @@ train_data, test_data = DataLoader(train_dataset, BATCH_SIZE), DataLoader(
 class NN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Sequential(nn.ZeroPad2d(1), nn.Conv2d(3, 32, 3), nn.ReLU())
-        self.conv2 = nn.Sequential(nn.ZeroPad2d(1), nn.Conv2d(32, 32, 3), nn.ReLU(),)
+        self.conv1 = nn.Sequential(nn.ZeroPad2d(1), nn.Conv2d(3, 32, 3), nn.BatchNorm2d(32), nn.ReLU())
+        self.conv2 = nn.Sequential(nn.ZeroPad2d(1), nn.Conv2d(32, 32, 3),nn.BatchNorm2d(32), nn.ReLU(),)
         self.pool1 = nn.MaxPool2d(2)
-        self.conv3 = nn.Sequential(nn.ZeroPad2d(1), nn.Conv2d(32, 64, 3), nn.ReLU())
-        self.conv4 = nn.Sequential(nn.ZeroPad2d(1), nn.Conv2d(64, 64, 3), nn.ReLU())
+        self.conv3 = nn.Sequential(nn.ZeroPad2d(1), nn.Conv2d(32, 64, 3),nn.BatchNorm2d(64), nn.ReLU())
+        self.conv4 = nn.Sequential(nn.ZeroPad2d(1), nn.Conv2d(64, 64, 3),nn.BatchNorm2d(64), nn.ReLU())
         self.pool2 = nn.MaxPool2d(2)
-        self.conv5 = nn.Sequential(nn.ZeroPad2d(1), nn.Conv2d(64, 128, 3), nn.ReLU())
-        self.conv6 = nn.Sequential(nn.ZeroPad2d(1), nn.Conv2d(128, 128, 3), nn.ReLU())
+        self.conv5 = nn.Sequential(nn.ZeroPad2d(1), nn.Conv2d(64, 128, 3),nn.BatchNorm2d(128), nn.ReLU())
+        self.conv6 = nn.Sequential(nn.ZeroPad2d(1), nn.Conv2d(128, 128, 3),nn.BatchNorm2d(128), nn.ReLU())
         self.pool3 = nn.MaxPool2d(2)
-        self.fc1 = nn.Sequential(nn.Linear(2048, 512), nn.ReLU())
-        self.drop = nn.Dropout(0.5)
-        self.fc2 = nn.Sequential(nn.Linear(512, 10))
+        self.fc1 = nn.Sequential(nn.Linear(2048, 1024), nn.ReLU())
+        self.drop1 = nn.Dropout(0.5)
+        self.fc2 = nn.Sequential(nn.Linear(1024, 512), nn.ReLU())
+        self.drop2 = nn.Dropout(0.5)
+        self.out = nn.Linear(512, 10)
 
 
     def forward(self, input):
@@ -64,14 +65,19 @@ class NN(nn.Module):
         x = self.pool3(x)
         
         x = torch.flatten(x, start_dim=1)
+
         x = self.fc1(x)
-        x = self.drop(x)
+        x = self.drop1(x)
+
         x = self.fc2(x)
+        x = self.drop2(x)
+
+        x = self.out(x)
         return x
 
 
 LR = 6e-4
-N_EPOCHS = 50
+N_EPOCHS = 20
 
 model = NN().to(device)
 optimizer = optim.Adam(model.parameters(), LR)
@@ -83,16 +89,28 @@ for epoch in range(N_EPOCHS):
     bar = Bar("Training", max=len(train_data))
     for step, (input, target) in enumerate(train_data):
         bar.next()
+
         input, target = input.to(device), target.to(device)
+
         output = model(input)
         output = output.to(device)
+
         optimizer.zero_grad()
         loss = loss_fn(output, target)
         loss = loss.to(device)
         loss.backward()
         optimizer.step()
     bar.finish()
+
     print(f"Loss: {loss.item()}")
+    
+    accuracy = 0
+    for values, label in zip(output,target):
+        if torch.argmax(values) == label:
+            accuracy += 1       
+    print(f"train accuracy: {accuracy / BATCH_SIZE * 100}%")
+    
+
     with torch.no_grad():
         model.eval()
         accuracies = []
@@ -104,5 +122,5 @@ for epoch in range(N_EPOCHS):
                 if torch.argmax(values) == label:
                     accuracies[-1] += 1
         avg_accuracy = mean(accuracies) / 10
-        print(f"test accuracy: {avg_accuracy}")
+        print(f"test accuracy: {avg_accuracy}%")
     print("-" * 20) 
