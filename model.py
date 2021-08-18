1163694d2f35746143c2357c873608f207ade6fa
Wasymir <72921498+Wasymir@users.noreply.github.com>
2021-08-18 23:40:18 +0200

    change README.md file

--- model.py
+++ model.py
@@ -19,27 +19,26 @@ class NN(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Sequential(
-            nn.ZeroPad2d(1), nn.Conv2d(3, 32, 3), nn.BatchNorm2d(32), nn.ReLU()
+            nn.Conv2d(3, 32, 3, padding=1), nn.BatchNorm2d(32), nn.ReLU()
         )
         self.conv2 = nn.Sequential(
-            nn.ZeroPad2d(1),
-            nn.Conv2d(32, 32, 3),
+            nn.Conv2d(32, 32, 3, padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
         )
         self.pool1 = nn.MaxPool2d(2)
         self.conv3 = nn.Sequential(
-            nn.ZeroPad2d(1), nn.Conv2d(32, 64, 3), nn.BatchNorm2d(64), nn.ReLU()
+            nn.Conv2d(32, 64, 3,padding=1), nn.BatchNorm2d(64), nn.ReLU()
         )
         self.conv4 = nn.Sequential(
-            nn.ZeroPad2d(1), nn.Conv2d(64, 64, 3), nn.BatchNorm2d(64), nn.ReLU()
+            nn.Conv2d(64, 64, 3, padding=1), nn.BatchNorm2d(64), nn.ReLU()
         )
         self.pool2 = nn.MaxPool2d(2)
         self.conv5 = nn.Sequential(
-            nn.ZeroPad2d(1), nn.Conv2d(64, 128, 3), nn.BatchNorm2d(128), nn.ReLU()
+            nn.Conv2d(64, 128, 3, padding=1), nn.BatchNorm2d(128), nn.ReLU()
         )
         self.conv6 = nn.Sequential(
-            nn.ZeroPad2d(1), nn.Conv2d(128, 128, 3), nn.BatchNorm2d(128), nn.ReLU()
+            nn.Conv2d(128, 128, 3, padding=1), nn.BatchNorm2d(128), nn.ReLU()
         )
         self.pool3 = nn.MaxPool2d(2)
         self.fc1 = nn.Sequential(nn.Linear(2048, 1024), nn.ReLU())
