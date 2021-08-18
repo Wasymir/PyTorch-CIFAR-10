330acf92589aa9befc93c1bd40716edf6eeee9e4
Wasymir <72921498+Wasymir@users.noreply.github.com>
2021-08-18 23:37:53 +0200

    moved paddings into conv layers, moved dataloader,  datasets, training loop, etc. into  __name__ == '__main__'if & updated requirements.txt file

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-from numpy import mod
 import torch
 from torch import nn
 from torch import optim
@@ -13,31 +12,7 @@ from torchvision.transforms import (
 from torch.utils.data import DataLoader
 from statistics import mean
 from progress.bar import Bar
-
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
-AUGMENTATION_PROBABILITY = 0.5
-
-train_transform = Compose(
-    [
-        RandomHorizontalFlip(AUGMENTATION_PROBABILITY),
-        RandomVerticalFlip(AUGMENTATION_PROBABILITY),
-        ToTensor(),
-    ]
-)
-
-
-train_dataset = CIFAR10(
-    root="data", download=True, train=True, transform=train_transform
-)
-
-test_dataset = CIFAR10(root="data", download=True, train=False, transform=ToTensor())
-
-BATCH_SIZE = 1000
-
-train_data = DataLoader(train_dataset, BATCH_SIZE, shuffle=True)
-test_data = DataLoader(test_dataset, BATCH_SIZE, shuffle=True)
+from os.path import join
 
 
 class NN(nn.Module):
@@ -98,51 +73,78 @@ class NN(nn.Module):
         x = self.out(x)
         return x
 
+if __name__ == '__main__':
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
-LR = 6e-4
-N_EPOCHS = 30
+    AUGMENTATION_PROBABILITY = 0.5
 
-model = NN().to(device)
-optimizer = optim.Adam(model.parameters(), LR)
-loss_fn = nn.CrossEntropyLoss()
+    train_transform = Compose(
+        [
+            RandomHorizontalFlip(AUGMENTATION_PROBABILITY),
+            RandomVerticalFlip(AUGMENTATION_PROBABILITY),
+            ToTensor(),
+        ]
+    )
 
-for epoch in range(N_EPOCHS):
-    model.train()
-    print(f"Epoch: {epoch + 1} / {N_EPOCHS}")
-    bar = Bar("Training", max=len(train_data))
-    for step, (input, target) in enumerate(train_data):
-        bar.next()
 
-        input, target = input.to(device), target.to(device)
+    train_dataset = CIFAR10(
+        root="data", download=True, train=True, transform=train_transform
+    )
 
-        output = model(input)
-        output = output.to(device)
+    test_dataset = CIFAR10(root="data", download=True, train=False, transform=ToTensor())
 
-        optimizer.zero_grad()
-        loss = loss_fn(output, target)
-        loss = loss.to(device)
-        loss.backward()
-        optimizer.step()
-    bar.finish()
+    BATCH_SIZE = 1000
 
-    print(f"Loss: {loss.item()}")
+    train_data = DataLoader(train_dataset, BATCH_SIZE, shuffle=True)
+    test_data = DataLoader(test_dataset, BATCH_SIZE, shuffle=True)
 
-    accuracy = 0
-    for values, label in zip(output, target):
-        if torch.argmax(values) == label:
-            accuracy += 1
-    print(f"train accuracy: {accuracy / BATCH_SIZE * 100}%")
+    LR = 6e-4
+    N_EPOCHS = 30
 
-    with torch.no_grad():
-        model.eval()
-        accuracies = []
-        for input, target in test_data:
+    model = NN().to(device)
+    optimizer = optim.Adam(model.parameters(), LR)
+    loss_fn = nn.CrossEntropyLoss()
+
+    for epoch in range(N_EPOCHS):
+        model.train()
+        print(f"Epoch: {epoch + 1} / {N_EPOCHS}")
+        bar = Bar("Training", max=len(train_data))
+        for step, (input, target) in enumerate(train_data):
+            bar.next()
+
             input, target = input.to(device), target.to(device)
-            accuracies.append(0)
+
             output = model(input)
-            for values, label in zip(output, target):
-                if torch.argmax(values) == label:
-                    accuracies[-1] += 1
-        avg_accuracy = mean(accuracies) / 10
-        print(f"test accuracy: {avg_accuracy}%")
-    print("-" * 20)
+            output = output.to(device)
+
+            optimizer.zero_grad()
+            loss = loss_fn(output, target)
+            loss = loss.to(device)
+            loss.backward()
+            optimizer.step()
+        bar.finish()
+
+        print(f"Loss: {loss.item()}")
+
+        accuracy = 0
+        for values, label in zip(output, target):
+            if torch.argmax(values) == label:
+                accuracy += 1
+        print(f"train accuracy: {accuracy / BATCH_SIZE * 100}%")
+
+        with torch.no_grad():
+            model.eval()
+            accuracies = []
+            for input, target in test_data:
+                input, target = input.to(device), target.to(device)
+                accuracies.append(0)
+                output = model(input)
+                for values, label in zip(output, target):
+                    if torch.argmax(values) == label:
+                        accuracies[-1] += 1
+            avg_accuracy = mean(accuracies) / 10
+            print(f"test accuracy: {avg_accuracy}%")
+        print("-" * 20)
+
+    PATCH = ''
+    torch.save(model.state_dict(), join(PATCH, 'save.pt'))
